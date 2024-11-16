5cc3b30dc5bde951ac846b5d045895cbce3bf94b
Kandigos <dodamaycry@gmail.com>
2024-11-16 20:41:42 +0200

    Add files via upload

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -14,7 +14,7 @@ export default function Features() {
             <MapPin className="w-12 h-12 text-blue-500 mb-4" />
             <h3 className="text-2xl font-bold mb-4">איפה?</h3>
             <p className="text-lg text-gray-600">
-              {content.schedule.location}
+              פרדס חנה, מול מחנה 80
               <a 
                 href="https://waze.com/ul/hsvbbsj4vy"
                 target="_blank"
